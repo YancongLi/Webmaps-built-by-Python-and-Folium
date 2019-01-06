803f77585346a1482b15f981a2291007ebf98807
Peter <yancong.li@alumni.ubc.ca>
2019-01-06 01:50:19 -0800

    Added stylized text in the popup window

--- map1.py
+++ map1.py
@@ -5,10 +5,15 @@ data = pandas.read_csv("Volcanoes_USA.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
+ 
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
 
-map = folium.Map(location=[43.07, -105.30],
-                 zoom_start=3, tiles='Mapbox Bright')
-
+map = folium.Map(location=[43.07, -105.30], zoom_start=3, tiles='Mapbox Bright')
 fg = folium.FeatureGroup(name="My Map")
 fg.add_child(folium.Marker(location=[
              49.2614, -123.2459], popup="I study Computer Science at UBC !", icon=folium.Icon(color='blue', icon='home')))
@@ -16,6 +21,13 @@ fg.add_child(folium.Marker(location=[
              49.8815, -119.4625], popup="My first Co-Op !", icon=folium.Icon(color='blue', icon='flag')))
 
 fgv = folium.FeatureGroup(name="Volcanoes_USA")
+
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "green")))
+ 
+map.add_child(fg)
+map.save("Map_html_popup_simple.html")
 
 
 def color_producer(elevation):
